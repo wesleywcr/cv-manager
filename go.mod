fe7c450dd5c316a8e0875b5851a4f5cfc90cde79
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2025-01-06 17:09:46 -0300

    feat: add chi as router

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.23.2
 
 require (
 	github.com/99designs/gqlgen v0.17.62
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/cors v1.2.1
 	github.com/vektah/gqlparser/v2 v2.5.21
 )
 
--- server.go
+++ server.go
@@ -8,11 +8,9 @@ import (
 	"gihub.com/wesleywcr/cv-manager/generated"
 	"gihub.com/wesleywcr/cv-manager/resolvers"
 	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/handler/extension"
-	"github.com/99designs/gqlgen/graphql/handler/lru"
-	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/vektah/gqlparser/v2/ast"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
 const defaultPort = "8080"
@@ -23,22 +21,22 @@ func main() {
 		port = defaultPort
 	}
 
+	router := chi.NewRouter()
+	router.Use(
+		cors.Handler(
+			cors.Options{
+				AllowedOrigins:   []string{"*"}, //any address can access
+				AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodOptions},
+				AllowCredentials: true,
+			},
+		),
+	)
+
 	srv := handler.New(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers.Resolver{}}))
 
-	srv.AddTransport(transport.Options{})
-	srv.AddTransport(transport.GET{})
-	srv.AddTransport(transport.POST{})
-
-	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
-
-	srv.Use(extension.Introspection{})
-	srv.Use(extension.AutomaticPersistedQuery{
-		Cache: lru.New[string](100),
-	})
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
